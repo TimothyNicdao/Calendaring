bb3029f8cfbcb90eb681c9c86df1e9bf92debda6
TimothyNicdao <tmnicdao@hawaii.edu>
2020-07-10 11:19:15 -1000

    Updates

---
+++ precache-manifest.71d26fb99f1d30c38cdd161d3bf8c051.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611f116e0d55fd75dcc98b37a0b310d0",
+    "url": "/Calendaring/index.html"
+  },
+  {
+    "revision": "2e9944a44ced3a5cf6f4",
+    "url": "/Calendaring/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "686fd8c81a935dfbbcdd",
+    "url": "/Calendaring/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e9944a44ced3a5cf6f4",
+    "url": "/Calendaring/static/js/2.50ff6f07.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/Calendaring/static/js/2.50ff6f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686fd8c81a935dfbbcdd",
+    "url": "/Calendaring/static/js/main.fa94694e.chunk.js"
+  },
+  {
+    "revision": "15bc0e6242cfa44ced68",
+    "url": "/Calendaring/static/js/runtime-main.9fad581f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calendaring/static/media/logo.5d5d9eef.svg"
+  }
+]);
